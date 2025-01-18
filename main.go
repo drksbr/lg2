1a0d0674e3d7da1edc3395ad96a3dfb64ec75fab
drksbr <isaacramonpb@gmail.com>
2025-01-18 13:16:32 -0300

    Remover código não utilizado e ajustar a inicialização da interface TUI para usar a consulta da CLI

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/drksbr/lg2/pkg/cli"
-	"github.com/drksbr/lg2/pkg/fetch"
-	"github.com/drksbr/lg2/pkg/parser"
 	"github.com/drksbr/lg2/pkg/tui"
 )
 
@@ -13,17 +9,7 @@ func main() {
 	// Obter os argumentos da CLI
 	query := cli.ParseArgs()
 
-	// Buscar dados do Looking Glass
-	htmlData, err := fetch.GetLookingGlassData(query)
-	if err != nil {
-		fmt.Printf("Erro ao buscar dados: %v\n", err)
-		return
-	}
-
-	// Processar os resultados
-	results, _ := parser.ParseHTML(htmlData, query)
-
 	// // Exibir interface interativa
-	t := tui.NewTUI(results)
+	t := tui.NewTUI(query)
 	t.Start()
 }
