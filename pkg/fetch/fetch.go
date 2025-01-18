ae5b89c5089c60df8c8d527f885f5f353f9e5da7
drksbr <isaacramonpb@gmail.com>
2025-01-18 09:10:29 -0300

    Adicionar funcionalidade para salvar dados HTML em arquivo para Debug e atualizar a requisição HTTP no Looking Glass

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -1,34 +1,65 @@
 package fetch
 
 import (
-	"bytes"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 var (
-	debug bool   = false
-	url   string = "https://bgp.tools/super-lg"
+	debug      bool   = false
+	saveSample bool   = false
+	url        string = "https://lg.ring.nlnog.net/prefix"
 )
 
-// GetLookingGlassData faz a requisição HTTP para o Looking Glass.
-func GetLookingGlassData(query string) (string, error) {
-
-	if debug {
-		url = "http://localhost:3000/lg"
+// Func to save html data to a file
+func saveHTML(htmlData string) {
+	// Create samples directory if it doesn't exist
+	if err := os.MkdirAll("samples", 0755); err != nil {
+		log.Fatal(err)
 	}
-	data := "q=" + query + "&asnmatch="
-	headers := map[string]string{
-		"accept":       "text/html",
-		"content-type": "application/x-www-form-urlencoded",
-		"origin":       "https://bgp.tools",
-		"user-agent":   "Mozilla/5.0 (compatible; LG2/1.0)",
+	timestamp := time.Now().Format("20060102-150405")
+	f, err := os.Create(fmt.Sprintf("samples/sample-%s.html", timestamp))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err = f.WriteString(htmlData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// GetLookingGlassData makes an HTTP request to the Looking Glass.
+func GetLookingGlassData(query string) (string, error) {
+	if debug {
+		url = "http://localhost:49445/sample"
 	}
 
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(data))
+	reqURL := url + "?q=" + query + "&match=exact&peer=all"
+	req, err := http.NewRequest("GET", reqURL, nil)
 	if err != nil {
 		return "", err
+	}
+
+	headers := map[string]string{
+		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"Accept-Language":           "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6",
+		"Cache-Control":             "max-age=0",
+		"Connection":                "keep-alive",
+		"Sec-Fetch-Dest":            "document",
+		"Sec-Fetch-Mode":            "navigate",
+		"Sec-Fetch-Site":            "none",
+		"Sec-Fetch-User":            "?1",
+		"Upgrade-Insecure-Requests": "1",
+		"User-Agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+		"sec-ch-ua":                 "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+		"sec-ch-ua-mobile":          "?0",
+		"sec-ch-ua-platform":        "\"macOS\"",
 	}
 
 	for key, value := range headers {
@@ -44,6 +75,11 @@ func GetLookingGlassData(query string) (string, error) {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
+	}
+
+	// Save sample HTML data to a file
+	if saveSample {
+		saveHTML(string(body))
 	}
 
 	return string(body), nil
