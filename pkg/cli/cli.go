e79743da5afcd95991f28faf80a01387c6dce769
drksbr <isaacramonpb@gmail.com>
2025-01-18 14:32:50 -0300

    Adicionar interface TUI para o CLI do Looking Glass, melhorando a usabilidade e a interação com consultas BGP

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,42 +1,64 @@
 package cli
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
-	"github.com/drksbr/lg2/pkg/parser"
+	"github.com/drksbr/lg2/pkg/config"
+	"github.com/drksbr/lg2/pkg/tui"
+	"github.com/spf13/cobra"
 )
 
-// ParseArgs lida com argumentos da linha de comando.
-func ParseArgs() string {
-	flag.Usage = func() {
-		fmt.Println("Uso: lg <prefix>")
-		fmt.Println("Exemplo IPv4: 1.1.1.0/24 ou 1.1.1.0")
-		fmt.Println("Exemplo IPv6: 2001:db8::/32 ou 2001:db8::")
-		flag.PrintDefaults()
+var (
+	Banner = `
+    ▒▒  ▒▓ ▒▒▒▒▓  ▓▒▒▒▒▒
+   ▓  ▓ ▒ ▒ ▒   ▓▒    ▒ 
+  ▓   ▓▓   ▒ ▒  ▒ ▒  ▒     Multiglass %s // (c) 2024
+ ▓▒▒▒▓▒ ▓▒▒▒▓ ▒▒▒  ▒▒      https://github/drksbr/lg2
+
+-> A TUI interface for nlnog.net looking glass, essential for querying and analyzing BGP routes, providing insights on AS-PATH, origin validations, and communities in an interactive and intuitive manner. Compatible with Windows, Mac, and Linux, it offers high performance and an optimized interface for network engineers and administrators.
+
+	IPv4 usage example: lg 1.1.1.0/24
+	IPv6 usage example: lg 2001:db8::/32`
+
+	showVersion bool
+
+	rootCmd = &cobra.Command{
+		Use:   "lg [flags] [prefix]",
+		Short: "Looking Glass CLI for querying BGP prefixes",
+		Long:  fmt.Sprintf(Banner, config.Version),
+		Run:   run,
+	}
+)
+
+func run(cmd *cobra.Command, args []string) {
+	if showVersion {
+		fmt.Printf("lg version %s\n", config.Version)
+		return
 	}
 
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) != 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	prefix := args[0]
-
-	network, err := parser.GetNetworkFromPrefix(prefix)
-	if err != nil {
+	if err := searchPrefix(cmd, args); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
 
-	if network == nil {
-		fmt.Println("Prefixo inválido")
-		os.Exit(1)
+func searchPrefix(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("é necessário informar um prefixo")
 	}
 
-	return prefix
+	// // Exibir interface interativa
+	t := tui.NewTUI(args[0])
+	t.Start()
+
+	return nil
+}
+
+func Execute() {
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "mostra a versão do lg")
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
