8865f4779f14d3e1137cabe6a369a5deb6928b4b
drksbr <isaacramonpb@gmail.com>
2025-01-18 14:32:31 -0300

    Remover lógica de inicialização da TUI e substituir por execução direta da CLI

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"github.com/drksbr/lg2/pkg/cli"
-	"github.com/drksbr/lg2/pkg/tui"
 )
 
 func main() {
-	// Obter os argumentos da CLI
-	query := cli.ParseArgs()
-
-	// // Exibir interface interativa
-	t := tui.NewTUI(query)
-	t.Start()
+	cli.Execute()
 }
